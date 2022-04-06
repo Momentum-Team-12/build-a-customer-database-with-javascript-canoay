b8ac018504135fb7256b21c0cd0f1291d65d3029
Ambar Cano <ambarcano@gmail.com>
2022-04-05 20:39:26 -0400

    basically done

--- main.js
+++ main.js
@@ -1,44 +1,36 @@
-console.log(customers)
- 
- // <h1>Customer Directory</h1>
-    // <div class="container">
-    //     <div class="customer">
-    //         <img src="https://randomuser.me/api/portraits/med/women/62.jpg" alt="Sophia Burns">
-    //         <h2>Sophia Burns</h2>
-    //         <div>info
-    //         <div>Email</div>
-    //         <div>Address</div>
-    //         <div>DOB</div>
-    //         <div>Customer Since</div>
-    //     </div>
-    // </div>
-    // </div>
+function capitalizeLetter (word) {
+    return word.slice(0,1).toUpperCase() + word.slice(1)
+}
 
-// asking the js to build one of these for each customer
 
 const allCustomersDiv = document.querySelector('#allCustomers')
 
+
 for (let customer of customers) {
-    console.log(`${customer.name.first} ${customer.name.last}`)
     let custDiv = document.createElement('div')
     custDiv.classList.add("person")
-    // let pic = document.createElement('img')
-    // pic.innerText = customer.picture.medium
-    let nameEl = document.createElement('h3')
-    nameEl.innerText = `${customer.name.first} ${customer.name.last}`
+    let imageEl = document.createElement('img')
+    imageEl.src = customer.picture.large
+    let nameEl = document.createElement('h4')
+    nameEl.innerText = `${capitalizeLetter(customer.name.first)} ${capitalizeLetter(customer.name.last)}`
     let custEmail = document.createElement('p')
-    // custEmail.classList.add("emailFromCustomer")
+    custEmail.classList.add("email")
     custEmail.innerText = `${customer.email}`
     let custAddress = document.createElement('p')
-    custAddress.innerText = `${customer.location.street.number} ${customer.location.street.name}, ${customer.location.city} ${customer.location.state} ${customer.location.postcode}`
+    custAddress.innerText = `${customer.location.street.number} ${customer.location.street.name}
+     ${customer.location.city},  ${nameToAbbr(customer.location.state)} ${customer.location.postcode}`
     let custDOB = document.createElement('p')
-    custDOB.innerText = `${customer.dob.date}`
-    // custDiv.appendChild(pic)
+    custDOB.innerText = `DOB: ${moment(customer.dob.date).format("MMM Do, YYYY")}`
+     let regDate = document.createElement('p')
+    regDate.innerText = `Customer since: ${moment(customer.registered.date).format("MMM Do, YYYY")}`
+
     allCustomersDiv.appendChild(custDiv)
+    custDiv.appendChild(imageEl)
     custDiv.appendChild(nameEl)
     custDiv.appendChild(custEmail)
     custDiv.appendChild(custAddress)
-     custDiv.appendChild(custDOB)
+    custDiv.appendChild(custDOB)
+    custDiv.appendChild(regDate)
     
 }
 
